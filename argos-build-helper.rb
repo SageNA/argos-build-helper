384cfcf411b847ca611da86b39528c118a43fa50
mmorton <mmorton@icanhascode.com>
2011-09-02 16:54:35 -0700

    first pass at dependency resolution.

--- argos-build-helper.rb
+++ argos-build-helper.rb
@@ -8,7 +8,27 @@ def is_interesting_file(file)
   file_name =~ /\.js$/i
 end
 
+def resolve_dependencies(mod, lookup)
+  chain = []
+  for import in mod.imports
+    chain << resolve_dependencies(lookup[import], lookup) if lookup[import]
+    chain << import
+  end
+  return chain
+end
+
 inspector = DocJS::Inspectors::DojoAmdInspector.new()
 meta = inspector.inspect_path('C:\Development\DojoSandbox', true) do |file| is_interesting_file(file) end
+
+module_lookup = {}
+
+meta.modules do |mod|
+  module_lookup[mod.name] = mod
+end
+
+locate = module_lookup['Sage/One']
+resolved = resolve_dependencies(locate, module_lookup).flatten.uniq
+
+print resolved
 
 print meta
